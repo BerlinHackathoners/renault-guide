a198bb95a51c334304a98a643440155fdb9b3b0d
Khonsu Development <khonsu.development@gmail.com>
2018-05-25 17:10:55 +0200

    Improved consumer experience

---
+++ src/main/java/com/renault/guide/intent/IntentModule.java
@@ -0,0 +1,7 @@
+package com.renault.guide.intent;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IntentModule {
+}
---
+++ src/main/java/com/renault/guide/map/MapModule.java
@@ -0,0 +1,7 @@
+package com.renault.guide.map;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MapModule {
+}
